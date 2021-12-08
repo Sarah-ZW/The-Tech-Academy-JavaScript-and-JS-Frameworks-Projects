406b6674b08d604b3134b942b25ca3113b17631a
Sarah-ZW <earthclaninfo@gmail.com>
2021-12-08 16:41:27 -0500

    React connect four

---
+++ React/connect-four/src/components/PlaySound.js
@@ -0,0 +1,25 @@
+import Sound from 'react-sound';
+import Music1 from './Sounds/music1.mp3';
+
+
+const PlaySound = (
+    handleSongLoading,
+    handleSongPlaying,
+    handleSongFinishedPlaying
+) => {
+    return(
+        <div>
+            <Sound
+            url={Music1}
+            playStatus={Sound.status.PLAYING}
+            playFromPosition={300}
+            onLoading={handleSongLoading}
+            onPlaying={handleSongPlaying}
+            onFinishedPlaying={handleSongFinishedPlaying}
+            loop= {true}
+            />
+        </div>
+    );
+};
+
+export default PlaySound;
--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './Mysass.scss';
 
+class MyHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Style!</h1>
+        <p> Add a little style!.</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyHeader />, document.getElementById('root1'));
 ReactDOM.render(
   <React.StrictMode>
     <App />
